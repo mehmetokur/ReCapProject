538c7e799cf39097fb10d0be07b9072f737861e3
mehmetokur <77415001+mehmetokur@users.noreply.github.com>
2021-02-01 02:20:15 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,57 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new CarDal());
+            Console.WriteLine("................GetAll.................");
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine("Araç adları :"+" "+car.Description);
+            }
+            Console.WriteLine("...............Car Added...............");
+            Car car6 = new Car()
+            {
+                Id = 6,
+                BrandId = 6,
+                ColorId = 1,
+                DailyPrice = 500,
+                ModelYear = 2015,
+                Description = "Toyota Corolla"
+            };
+            carManager.Add(car6);
+            Console.WriteLine("...............New GetAll...............");
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine("Araç adları :" + " " + car.Description + " " + "Araç Fiyatı :" +" "+ car.DailyPrice);
+            }
+            Console.WriteLine("................GetById................");
+            Car car1 = carManager.GetById(1);
+            Console.WriteLine(car1.Id);
+            
+            Console.WriteLine("................Uptaded.................");
+
+            car6.DailyPrice = 1000;
+            carManager.Update(car6);
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine("Araç adları :" + " " + car.Description+" "+"Araç Fiyatı:"+" "+car.DailyPrice);
+            }
+            Console.WriteLine("................Deleted.................");
+            carManager.Delete(car6);
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine("Araç adları :" + " " + car.Description);
+            }
+            
+           
+
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/CarDal.cs
@@ -0,0 +1,60 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    
+   public class CarDal : ICarDal
+    {
+        List<Car> _cars;
+
+        public CarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car(){Id=1,BrandId=1,ColorId=2,ModelYear=2017,DailyPrice=350,Description="Ford Connect 2017"},
+                new Car(){Id=2,BrandId=2,ColorId=2,ModelYear=2005,DailyPrice=150,Description="BMW x5 2005"},
+                new Car(){Id=3,BrandId=3,ColorId=2,ModelYear=2021,DailyPrice=650,Description="Fiat Egea Sedan"},
+                new Car(){Id=4,BrandId=4,ColorId=2,ModelYear=2013,DailyPrice=250,Description="Ford focus"},
+                new Car(){Id=5,BrandId=5,ColorId=2,ModelYear=2009,DailyPrice=180,Description="wolkswagen Gold"}
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+            Console.WriteLine("Car Added.");
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public Car GetById(int Id)
+        {
+            return _cars.SingleOrDefault(c => c.Id == Id);
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUptade = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUptade.Id = car.Id;
+            carToUptade.BrandId = car.BrandId;
+            carToUptade.ColorId = car.ColorId; ;
+            carToUptade.DailyPrice = car.DailyPrice;
+            carToUptade.Description = car.Description;
+            carToUptade.ModelYear = car.ModelYear;
+        }
+    }
+}
